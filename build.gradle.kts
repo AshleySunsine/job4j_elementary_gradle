b6d045da336a3015698c316e85cc7b176bd4f940
AshleySunsine <sgt16@yandex.ru>
2025-09-27 00:30:39 +0300

    Жизненный цикл сборки Gradle [#505351 #552452]

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    testImplementation(libs.org.junit.jupiter.junit.jupiter.engine)
+    testImplementation(libs.org.junit.vintage.junit.vintage.engine)
+    testImplementation(libs.org.assertj.assertj.core)
+}
+
+group = "ru.job4j"
+version = "1.0"
+description = "elementary"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.register<Zip>("zipJavaDoc") {
+    group = "documentation" // Группа, в которой будет отображаться задача
+    description = "Packs the generated Javadoc into a zip archive"
+
+    dependsOn("javadoc") // Указываем, что задача зависит от выполнения javadoc
+
+    from("build/docs/javadoc") // Исходная папка для упаковки
+    archiveFileName.set("javadoc.zip") // Имя создаваемого архива
+    destinationDirectory.set(layout.buildDirectory.dir("archives")) // Директория, куда будет сохранен архив
+}
